e0914989fa871038a8e398803623dcd74dffbcfa
Nick Bechtel <bechtel.nick@gmail.com>
2021-10-14 11:20:04 -0400

    commit tree 81cd669664f96c0a3dd58c0fdbf3a27e83e99bf2

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+var bestCustomer;
+
+const leastFavoriteCustomer = "nick";
+
+function upperCaseCustomerName() {
+  customerName = "BOB";
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "kana";
+}
